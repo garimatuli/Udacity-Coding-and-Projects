7e1ae782d6729aeb3bd0ee34d61cc34df36a76eb
garimatuli <gtraipur@gmail.com>
2020-07-08 16:43:12 -0400

    Useful String Methods

--- Python-Strings-Lists-Style-Structure/string_Methods.py
+++ Python-Strings-Lists-Style-Structure/string_Methods.py
@@ -3,6 +3,7 @@
 # Locate substring in a string -> find
 # Count substring in a string -> count
 # join a substring in string -> join
+# split a string with any character ch -> str.split("ch")
 
 print('cad' in 'abracadabra')  # True
 print('gar' in 'abracadabra')  # False
@@ -22,3 +23,29 @@ lines = ["Haiku frogs in snow",
          "Tetrametric drum-beats thrumming, Hiawathianic rhythm."]
 
 print('<br>'.join(lines))
+print("\n")
+
+
+def extract_place_with_split(filename):
+    arr = filename.split("_")
+    return arr[1]
+
+
+# Here are some calls you can test it with:
+print(extract_place_with_split("2016-11-04_Berlin_09/42/22.jpg"))
+print(extract_place_with_split("2018-01-03_Oahu_21/51/57.jpg"))
+print(extract_place_with_split("2018-01_Scottland_11/51/27.jpg"))
+
+
+def extract_place_with_slicing(filename):
+    first_ = filename.find('_')
+    partial_result = filename[first_ + 1:]
+    second_ = partial_result.find('_')
+    result = partial_result[:second_]
+    return result
+
+
+# Here are some calls you can use for testing:
+print(extract_place_with_slicing("2016-11-04_Berlin_09/42/22.jpg"))
+print(extract_place_with_slicing("2018-01-03_Oahu_21/51/57.jpg"))
+print(extract_place_with_slicing("2018-01_Scottland_11/51/27.jpg"))
